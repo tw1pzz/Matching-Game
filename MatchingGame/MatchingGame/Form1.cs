2dafb795dbe704cf088d2ccf80069ebb62cf2b39
tw1pzz <97695053+tw1pzz@users.noreply.github.com>
2022-03-29 11:23:33 +0300

    Add files via upload

--- MatchingGame/MatchingGame/Form1.cs
+++ MatchingGame/MatchingGame/Form1.cs
@@ -12,6 +12,8 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        Label firstClicked = null;
+        Label secondClicked = null;
         Random random = new Random();
 
         List<string> icons = new List<string>()
@@ -24,6 +26,23 @@ namespace MatchingGame
             InitializeComponent();
             AssignIconsToSquares();
         }
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            MessageBox.Show("You matched all the icons!", "Congratulations");
+            Close();
+        }
+
         private void AssignIconsToSquares()
         {
             foreach (Control control in tableLayoutPanel1.Controls)
@@ -37,5 +56,57 @@ namespace MatchingGame
                 }
             }
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                
+
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
     }
+
 }
